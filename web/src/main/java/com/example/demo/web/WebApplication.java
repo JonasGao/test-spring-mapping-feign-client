a142ae95a521645aa145ee140e4f971bb07ea885
JonasGao <gaopinsong@live.cn>
2019-07-07 15:29:08 +0800

    update

--- web/src/main/java/com/example/demo/web/WebApplication.java
+++ web/src/main/java/com/example/demo/web/WebApplication.java
@@ -3,6 +3,7 @@ package com.example.demo.web;
 import org.apache.catalina.loader.WebappClassLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class WebApplication {
