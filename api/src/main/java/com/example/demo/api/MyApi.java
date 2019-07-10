2e83c88fe3346a7c507109c5d45619759f24eb03
Jonas <jonas.gao@voyageone.cn>
2019-07-10 10:47:03 +0800

    update
    add load balance debug log

--- api/src/main/java/com/example/demo/api/MyApi.java
+++ api/src/main/java/com/example/demo/api/MyApi.java
@@ -3,6 +3,6 @@ package com.example.demo.api;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public interface MyApi {
-    @GetMapping
+    @GetMapping("/api")
     String hello();
 }
